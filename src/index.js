ee5eed5a403a85881a0a6f71b9e74ef8de16890b
Richard Mands <richard@eduku.org>
2016-09-30 08:41:24 -0700

    ghpages?

--- src/index.js
+++ src/index.js
@@ -17,9 +17,9 @@ render(
   <Route path='/Stock-Market-App/' component={Layout}>
     
     <IndexRoute component={Welcome}></IndexRoute>
-    <Route path='/Stock-Market-App/stockPicker' component={StockPicker}></Route>
-    <Route path='/Stock-Market-App/stockListing' component={StockListing}></Route>
-    <Route path='/Stock-Market-App/stockQuote' component={StockQuote}></Route>
+    <Route path='/stockPicker' component={StockPicker}></Route>
+    <Route path='/stockListing' component={StockListing}></Route>
+    <Route path='/stockQuote' component={StockQuote}></Route>
     
   </Route>
 
